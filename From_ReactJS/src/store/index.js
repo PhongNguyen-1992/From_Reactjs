3ff7931456194ca7819156fac507956e17c94f14
PhongNguyen-1992 <phongnguyenfpt1992@gmail.com>
2025-07-17 22:22:56 +0700

    update_1

--- From_ReactJS/src/store/index.js
+++ From_ReactJS/src/store/index.js
@@ -4,13 +4,7 @@ import studentReducer from './studentSlice';
 const store = configureStore({
   reducer: {
     student: studentReducer
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
-      }
-    })
+  },  
 });
 
 export default store;
